c9cca134947d05e4b0b77297712919c06e671fa4
Jun Kim <jun.kim@cdk.com>
2018-10-26 12:03:37 -0700

    ssl update

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name Autofill Tool - 1.1.8
+// @name Autofill Tool - 1.1.9
 // @namespace www.cobaltgroup.com
-// @version 1.1.8
+// @version 1.1.9
 // @run-at document-end
 // @downloadURL https://github.com/cirept/autofillReplacer/raw/master/assets/js/meta.user.js
 // @resource toolStyles https://cdn.rawgit.com/cirept/autofillReplacer/1.1.8/assets/css/autofill.css
 // @resource changeLog https://cdn.rawgit.com/cirept/autofillReplacer/1.1.8/docs/LatestChanges.md
 // @resource toolInstructions https://cdn.rawgit.com/cirept/autofillReplacer/1.1.8/docs/README.md
-// @include http://websites.cobalt.com/wsm/editSite.do*
-// @include http://websites.cobalt.com/wsm/cms.do*
+// @include https://websites.cobalt.com/wsm/editSite.do*
+// @include https://websites.cobalt.com/wsm/cms.do*
 // @require https://code.jquery.com/jquery-3.3.1.min.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js
 // @require https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js
