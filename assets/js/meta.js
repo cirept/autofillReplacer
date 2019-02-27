1dcb8978b0e98e07c3afe481c9cbb567ed2a7b6f
Eric Tanaka <cirept612@gmail.com>
2019-02-26 16:18:27 -0800

    updated to 1.1.9-pre version

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name Autofill Tool - 1.1.9
+// @name Autofill Tool - 1.1.9-pre
 // @namespace www.cobaltgroup.com
-// @version 1.1.9
+// @version 1.1.9-pre
 // @run-at document-end
 // @downloadURL https://github.com/cirept/autofillReplacer/raw/master/assets/js/meta.user.js
-// @resource toolStyles https://cdn.rawgit.com/cirept/autofillReplacer/1.1.8/assets/css/autofill.css
-// @resource changeLog https://cdn.rawgit.com/cirept/autofillReplacer/1.1.8/docs/LatestChanges.md
-// @resource toolInstructions https://cdn.rawgit.com/cirept/autofillReplacer/1.1.8/docs/README.md
+// @resource toolStyles https://cdn.rawgit.com/cirept/autofillReplacer/1.1.9-pre/assets/css/autofill.css
+// @resource changeLog https://cdn.rawgit.com/cirept/autofillReplacer/1.1.9-pre/docs/LatestChanges.md
+// @resource toolInstructions https://cdn.rawgit.com/cirept/autofillReplacer/1.1.9-pre/docs/README.md
 // @include http://websites.cobalt.com/wsm/editSite.do*
 // @include https://websites.cobalt.com/wsm/editSite.do*
 // @include http://websites.cobalt.com/wsm/cms.do*
@@ -16,7 +16,7 @@
 // @require https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js
 // @require https://use.fontawesome.com/releases/v5.1.0/js/all.js
 // @require https://cdn.rawgit.com/showdownjs/showdown/1.8.6/dist/showdown.min.js
-// @require https://cdn.rawgit.com/cirept/autofillReplacer/1.1.8/assets/js/autofillTag.js
+// @require https://cdn.rawgit.com/cirept/autofillReplacer/1.1.9-pre/assets/js/autofillTag.js
 // @author cire.pt
 // @grant GM_getResourceURL
 // ==/UserScript==
